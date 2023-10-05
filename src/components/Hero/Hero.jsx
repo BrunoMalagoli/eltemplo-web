9641c4a4f3fadaed0ec79b140e06f76a73692f00
Bruno Malagoli <bmalagoli2@gmail.com>
2023-10-05 13:52:32 -0300

    NavBar position fixed and Hero background

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import HeroStyles from "./styles/index.module.css"
+
+
+const Hero = () => {
+    return(
+        <section id={HeroStyles.heroContainer}>
+            <div></div>
+        </section>
+    )
+}
+
+export default Hero
