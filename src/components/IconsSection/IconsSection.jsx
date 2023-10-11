91d9c110db57e394458510e6e9be6aca49cc1cdc
Bruno Malagoli <bmalagoli2@gmail.com>
2023-10-11 16:08:52 -0300

    Removing buttons on carousel (mobile)

--- src/components/IconsSection/IconsSection.jsx
+++ src/components/IconsSection/IconsSection.jsx
@@ -5,8 +5,8 @@ const IconsSection = () =>{
         <>
             <section id={IconsSectionStyles.iconsWrapper}>
                 <div id={IconsSectionStyles.iconsContainer}>
-                    <img src="/catena-logo.jpg" alt="Catena Zapata" height={"100px"} width={"200px"}/>
-                    <img src="/chandon-logo.jpg" alt="Chandon" height={"120px"} width={"200px"}/>
+                    <img src="/catena-logo.jpg" alt="Catena Zapata" height={"100px"} width={"250px"}/>
+                    <img src="/chandon-logo.jpg" alt="Chandon" height={"200px"} width={"200px"}/>
                     <img src="/rutini-logo.jpg" alt="Rutini Wines" height={"150px"} width={"200px"}/>
                     <img src="/casaspirque-logo.jpg" alt="Catena Zapata" height={"150px"} width={"200px"}/>
                 </div>
