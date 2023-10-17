204612d83cf54d9332044da109ba520706ee2a92
Bruno Malagoli <bmalagoli2@gmail.com>
2023-10-17 14:30:30 -0300

    First implementation of map component

--- src/components/NosotrosSection/NosotrosSection.jsx
+++ src/components/NosotrosSection/NosotrosSection.jsx
@@ -1,11 +1,15 @@
-
+import UbiMap from "./components/MapContainer";
+import NosotrosStyles from "./styles/index.module.css"
 
 const NosotrosSection = () =>{
     return(
     <>
-        <section>
-            <div>
-                Nosotros
+        <section id={NosotrosStyles.nosotrosWrapper}>
+            <div className={NosotrosStyles.nosotrosInfo}>
+
+            </div>
+            <div className={NosotrosStyles.map}>
+                <UbiMap/>
             </div>
         </section>
     </>
---
+++ src/components/NosotrosSection/components/MapContainer.jsx
@@ -0,0 +1,12 @@
+
+
+const MapContainer = () => {
+  return(
+    <>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.635668687947!2d-58.51255072492992!3d-34.588084172959974!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcb7b33fbbb6a3%3A0xd28cebce0f945c82!2sGriveo%203353%2C%20C1419EVW%20CABA!5e0!3m2!1ses-419!2sar!4v1697562961702!5m2!1ses-419!2sar" width="100%" height="100%" style={{border : 0}} allowFullScreen={false} loading="lazy"></iframe>
+    </>
+  )
+
+}
+
+export default MapContainer
