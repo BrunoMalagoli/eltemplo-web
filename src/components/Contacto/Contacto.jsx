010223f8558bd68e69850e7f8171a1eca2e2f9f3
Bruno Malagoli <bmalagoli2@gmail.com>
2023-10-18 16:17:00 -0300

    Contact section w/ styles implemented

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -1,9 +1,20 @@
+import IconWhatsapp from "./components/WhatsappIcon"
 import ContactoStyles from "./styles/index.module.css"
+import IconMail from "./components/IconMail"
 
 const Contacto = () =>{
+    const mailtoHref =
+    "mailto:eltemplodevoto@gmail.com?subject=Queremos Contactar!&body=Description";
     return(
-        <section>
-            
+        <section id={ContactoStyles.contactWrapper}>
+            <div className={ContactoStyles.contactItem}>
+                <IconWhatsapp heigth={"170px"} width={"170px"}/>
+                <button className={ContactoStyles.contactButtons}>Contactanos por Whatsapp</button>
+            </div>
+            <div className={ContactoStyles.contactItem}>
+                <IconMail heigth={"200px"} width={"200px"}/>
+                <button onClick={() => window.open(mailtoHref)} className={ContactoStyles.contactButtons}>Contactanos por Mail </button>
+            </div>
         </section>
     )
 
