3b67d494193f4bbfea718e743c630b43f4758aaa
Bruno Malagoli <bmalagoli2@gmail.com>
2023-10-19 14:33:57 -0300

    Footer creation, styles fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Contacto from './components/Contacto/Contacto'
+import Footer from './components/Footer/Footer'
 import Hero from './components/Hero/Hero'
 import IconsSection from './components/IconsSection/IconsSection'
 import NavBar from './components/NavBar/NavBar'
@@ -14,6 +15,7 @@ function App() {
         <IconsSection/>
         <NosotrosSection/>
         <Contacto/>
+        <Footer/>
     </>
   )
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import FooterStyles from "./styles/index.module.css"
+
+
+const Footer = () => {
+  return (
+    <footer id={FooterStyles.footerContainer}>
+        <div></div>
+        <div></div>
+    </footer>);
+};
+
+export default Footer;
